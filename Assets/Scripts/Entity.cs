62a535d19f9d2e311159fa444549f4c184064f9f
DESKTOP-J94IP15\Ben <benjamin.j.langerak@gmail.com>
2018-07-14 19:37:34 -0400

    added entity and entitydata classes for pulling character data in

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EntityData
+{
+	public string name;
+	public string description;
+
+	public int fight;
+	public int dash;
+	public int gusto;
+	public int currentHP;
+	public int maxHP;
+	public int bonusHP;
+	public int damage;
+	public int blockAmt;
+	public float critChance;
+	public float hitChance;
+	public float dodgeChance;
+
+	public int initiative;
+	public int threat;
+}
+
+public class Entity : MonoBehaviour {
+
+	public EntityData data;
+
+
+}
